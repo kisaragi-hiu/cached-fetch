13e475c99f0366c3fab3f14cd889a1f1fb3b9fad
Kisaragi Hiu <mail@kisaragi-hiu.com>
2025-11-03 03:46:58 +0900

    switch back to deno
    This allows us to use URL imports, to grab zx in testing for instance.
    
    t.step actually works as a way to write subtests despite
    /runtime/fundamentals/testing/ making it sound like it's (only) for
    setup and teardown.
    
    (describe from @std/testing/bdd does not support an async body, only
    test/it does, which is a very annoying limitation.)
    
    We also use @std/assert instead of @std/expect because the latter uses a
    slightly older version of @std/internal making the package graph more of
    a mess

--- index.test.ts
+++ index.test.ts
@@ -1,23 +1,24 @@
-import { describe, test, expect } from "bun:test";
+// -*- lsp-disabled-clients: (ts-ls); -*-
+import { assertEquals, assertLess } from "jsr:@std/assert";
 import { cached } from "./index.ts";
 
-describe("caching a fetch", async () => {
+Deno.test("caching a fetch", async (t) => {
   const key = "test1";
   let before: number | undefined = undefined;
   before = performance.now();
   const test1v1 = await cached(key, () => fetch("https://kisaragi-hiu.com"));
   const elapsed1 = performance.now() - before;
-  test("result is a string", () => {
-    expect(typeof test1v1).toBe("string");
+  await t.step("result is a string", () => {
+    assertEquals(typeof test1v1, "string");
   });
 
   before = performance.now();
   const test1v2 = await cached(key, () => fetch("https://kisaragi-hiu.com"));
   const elapsed2 = performance.now() - before;
-  test("result is a string when using the cache", () => {
-    expect(typeof test1v2).toBe("string");
+  await t.step("result is a string when using the cache", () => {
+    assertEquals(typeof test1v2, "string");
   });
-  test("caching is faster", () => {
-    expect(elapsed2).toBeLessThan(elapsed1);
+  await t.step("caching is faster", () => {
+    assertLess(elapsed2, elapsed1);
   });
 });
