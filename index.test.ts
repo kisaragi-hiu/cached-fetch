1deb5e95c7eb891e671ec285c981365bc0488ad2
Kisaragi Hiu <mail@kisaragi-hiu.com>
2025-11-03 03:15:58 +0900

    Write some tests with vitest
    I don't want to use Deno.test because I prefer the describe/test/expect
    pattern. So vitest it is.
    
    `deno task test` fails, so. I'm probably just moving away from Deno.

---
+++ index.test.ts
@@ -0,0 +1,23 @@
+import { describe, test, expect } from "vitest";
+import { cached } from "./index.ts";
+
+describe("caching a fetch", async () => {
+  const key = "test1";
+  let before: number | undefined = undefined;
+  before = performance.now();
+  const test1v1 = await cached(key, () => fetch("https://kisaragi-hiu.com"));
+  const elapsed1 = performance.now() - before;
+  test("result is a string", () => {
+    expect(typeof test1v1).toBe("string");
+  });
+
+  before = performance.now();
+  const test1v2 = await cached(key, () => fetch("https://kisaragi-hiu.com"));
+  const elapsed2 = performance.now() - before;
+  test("result is a string when using the cache", () => {
+    expect(typeof test1v2).toBe("string");
+  });
+  test("caching is faster", () => {
+    expect(elapsed2).toBeLessThan(elapsed1);
+  });
+});
