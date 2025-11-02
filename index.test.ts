db747025070fd548fdb602f866acadad97f48720
Kisaragi Hiu <mail@kisaragi-hiu.com>
2025-11-03 04:13:46 +0900

    test more cases

--- index.test.ts
+++ index.test.ts
@@ -1,8 +1,10 @@
 // -*- lsp-disabled-clients: (ts-ls); -*-
 import { assertEquals, assertLess } from "jsr:@std/assert";
+import { spawnSync } from "node:child_process";
+import { $ } from "npm:zx";
 import { cached } from "./index.ts";
 
-Deno.test("caching a fetch", async (t) => {
+Deno.test("fetch", async (t) => {
   const key = "test1";
   let before: number | undefined = undefined;
   before = performance.now();
@@ -18,6 +20,52 @@ Deno.test("caching a fetch", async (t) => {
   await t.step("result is a string when using the cache", () => {
     assertEquals(typeof test1v2, "string");
   });
+  await t.step("caching is faster", () => {
+    assertLess(elapsed2, elapsed1);
+  });
+});
+
+Deno.test("zx", async (t) => {
+  const key = "test2";
+  let before: number | undefined = undefined;
+
+  before = performance.now();
+  await t.step("result is the expected value", async () => {
+    assertEquals(await cached(key, () => $`echo hello`), "hello\n");
+  });
+  const elapsed1 = performance.now() - before;
+
+  before = performance.now();
+  await t.step("caching is faster", async () => {
+    await cached(key, () => $`echo hello`);
+    const elapsed2 = performance.now() - before;
+    assertLess(elapsed2, elapsed1);
+  });
+});
+
+Deno.test("sync", async (t) => {
+  const key = "test3";
+  let before: number | undefined = undefined;
+
+  function zxAtHome(command: string, args: string[]) {
+    return spawnSync(command, args, {
+      stdio: "pipe",
+      encoding: "utf-8",
+    });
+  }
+
+  before = performance.now();
+  await t.step("result is the expected value", () => {
+    assertEquals(
+      cached(key, () => zxAtHome("echo", ["hello"]).stdout),
+      "hello\n",
+    );
+  });
+  const elapsed1 = performance.now() - before;
+
+  before = performance.now();
+  cached(key, () => zxAtHome("echo", ["hello"]).stdout);
+  const elapsed2 = performance.now() - before;
   await t.step("caching is faster", () => {
     assertLess(elapsed2, elapsed1);
   });
